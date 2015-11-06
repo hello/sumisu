55f0a5e4addad9f3971dac903c08596522a90e5f
Jackson Chen <jchen@Jacksons-MacBook-Pro.local>
2015-11-06 15:59:13 -0800

    test build working

--- app/test/main.c
+++ app/test/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+#include "test.h"
 
 
 int main(int argc, char * argv[]){
     printf("hello world");
+    test();
     return 0;
 }
---
+++ platform/darwin/test.c
@@ -0,0 +1,7 @@
+#include "test.h"
+#include <stdio.h>
+
+void test(void){
+    printf("test\n");
+}
+
