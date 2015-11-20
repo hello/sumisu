e387fb948a9975923f374610bdd8ed137c909a84
Jackson Chen <jchen@sayhello.com>
2015-11-20 13:17:26 -0800

    bring up uart driver

--- platform/nrf52/io.c
+++ platform/nrf52/io.c
@@ -14,11 +14,15 @@ void uart_event_handler(nrf_drv_uart_event_t * p_event, void * p_context){
 
 void os_uart_init(void){
     nrf_drv_uart_config_t config = NRF_DRV_UART_DEFAULT_CONFIG;
-    ret_code_t ret = nrf_drv_uart_init(&config, uart_event_handler);
+    /*
+     *ret_code_t ret = nrf_drv_uart_init(&config, uart_event_handler);
+     */
+    ret_code_t ret = nrf_drv_uart_init(&config, NULL);
 }
 void os_printf(const char * format, ...){
     if(!uart_ready){
         os_uart_init();
         uart_ready = 1;
     }
+    nrf_drv_uart_tx("Hello", 4);
 }
