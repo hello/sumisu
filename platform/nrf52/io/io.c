dc19a3a1c520f3a93e6d8b2a5cd75501fb6f75fe
Jackson Chen <jchen@sayhello.com>
2015-11-23 13:03:04 -0800

    added input echo

--- platform/nrf52/io/io.c
+++ platform/nrf52/io/io.c
@@ -6,16 +6,26 @@
 #include "nrf_drv_uart.h"
 
 static int uart_ready;
+static uint8_t echo_buffer[1];
 
 void uart_event_handler(nrf_drv_uart_event_t * p_event, void * p_context){
+    switch(p_event->type){
+        case NRF_DRV_UART_EVT_TX_DONE:
+            break;
+        case NRF_DRV_UART_EVT_RX_DONE:
+            nrf_drv_uart_tx(echo_buffer, 1);
+            nrf_drv_uart_rx(echo_buffer, 1);
+            break;
+        default:
+        case NRF_DRV_UART_EVT_ERROR:
+            break;
+    }
 }
 
 void os_uart_init(void){
     nrf_drv_uart_config_t config = NRF_DRV_UART_DEFAULT_CONFIG;
-    /*
-     *ret_code_t ret = nrf_drv_uart_init(&config, uart_event_handler);
-     */
-    ret_code_t ret = nrf_drv_uart_init(&config, NULL);
+    ret_code_t ret = nrf_drv_uart_init(&config, uart_event_handler);
+    nrf_drv_uart_rx(echo_buffer, 1);
 }
 void os_printf(const char * format, ...){
     if(!uart_ready){
