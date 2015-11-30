02c05f2e537376a321793314953d39b636ce598e
Jackson Chen <jchen@sayhello.com>
2015-11-30 13:16:25 -0800

    updated test

--- app/test/main.c
+++ app/test/main.c
@@ -4,12 +4,33 @@
 #include "test.h"
 #include "dtm.h"
 #include "io.h"
+#include "cmsis_os.h"
 
+static void test1(void const * arg){
+    while(1){
+        os_printf("T1\r\n");
+        osDelay(1000);
+        os_printf("ET1\r\n");
+    }
+}
+static void test2(void const * arg){
+    while(1){
+        os_printf("R1\r\n");
+        osDelay(1000);
+    }
+}
 int main(int argc, char * argv[]){
-    os_uart_init();
-    os_printf("Welcome to sumisu\r\n");
-    osDelay(1000);
-    test_rtos();
+    osThreadDef_t t1 = (osThreadDef_t){
+        .name = "test1",
+        .pthread = test1,
+        .tpriority = 0,
+        .instances = 1,
+        .stacksize = 512,
+    };
+
+    osKernelInitialize();
+    osThreadCreate(&t1, 0);
+    osKernelStart();
     for(;;);
     return 0;
 }
