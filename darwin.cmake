ee168b117cd932f99dad635c5b028c72ff1b3d66
Jackson Chen <jchen@sayhello.com>
2015-11-30 15:50:19 -0800

    force 32bit build

--- darwin.cmake
+++ darwin.cmake
@@ -13,4 +13,7 @@ set(CMAKE_C_COMPILER /usr/bin/clang)
 set(CMAKE_C_FLAGS
     "-DBSP_DEFINES_ONLY"
     "-std=gnu99"
+    "-m32"
     )
+string(REGEX REPLACE ";" " " CMAKE_C_FLAGS "${CMAKE_C_FLAGS}")
+set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "")
