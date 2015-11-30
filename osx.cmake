7fca902da5fd4cbf599e8716ca37ff75ff07e1f3
Jackson Chen <jchen@sayhello.com>
2015-11-30 15:11:24 -0800

    simplified toolchain somewhat

--- osx.cmake
+++ osx.cmake
@@ -1,14 +1,16 @@
 #/* vim: set ft=cmake: */
 
-set(CMAKE_SYSTEM_NAME darwin)
 set(TARGET_PLATFORM darwin)
-
-##############################################
-# Download and Setup
-##############################################
-#file(DOWNLOAD ${DOWNLOAD_SOURCE} ${GCC_ROOT} SHOWPROGRESS COMMENT "Downloading ${GCC_ROOT}")
-#execute_process(COMMAND tar jxf ${GCC_ROOT} WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
+set(CMAKE_SYSTEM_NAME Generic)
 
 ##############################################
 # TOOLCHAIN(Use default)
 ##############################################
+
+#message(STATUS "Using toolchain: GCC")
+set(CMAKE_C_COMPILER /usr/bin/clang)
+
+set(CMAKE_C_FLAGS
+    "-DBSP_DEFINES_ONLY"
+    "-std=gnu99"
+    )
