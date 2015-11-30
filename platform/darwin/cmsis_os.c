add5ea39c43b4d41f1b9f9cdcaf8b7aab439cc7f
Jackson Chen <jchen@sayhello.com>
2015-11-30 15:51:41 -0800

    added simple posix

--- platform/darwin/cmsis_os.c
+++ platform/darwin/cmsis_os.c
@@ -1,1 +1,35 @@
+/**
+ * wip -jackson
+ * none of these adheres to the standard but will get the target running on the platform
+ */
+#include <sys/types.h>
+#include <pthread.h>
 #include "cmsis_os.h"
+#include "io.h"
+
+osStatus osKernelStart (void){
+    os_printf("Sumisu - Darwin\r\n");
+    return osOK;
+}
+osStatus osKernelInitialize (void){
+    return osOK;
+}
+osThreadId osThreadCreate (const osThreadDef_t *thread_def, void *argument){
+    pthread_t thread;
+    pthread_attr_t attr;
+    if(0 != pthread_attr_init(&attr)){
+        //error
+    }
+    pthread_attr_setstacksize(&attr, thread_def->stacksize);
+    int ret = pthread_create(
+            &thread,
+            &attr,
+            thread_def->pthread,
+            argument
+            );
+    if(0 == ret){
+        return 0;
+    }else{
+        return (osThreadId)thread;
+    }
+}
