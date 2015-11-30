1219bc741552858700627c4c05807fa617206521
Jackson Chen <jchen@sayhello.com>
2015-11-30 13:15:29 -0800

    added osKernelInitialize

---
+++ platform/nrf52/init.c
@@ -0,0 +1,16 @@
+#include "softdevice_handler.h"
+#include "cmsis_os.h"
+#include "io.h"
+#include "nrf_delay.h"
+
+osStatus osKernelInitialize (void){
+    //setup clksrc for kernel ticks
+    SOFTDEVICE_HANDLER_INIT(NRF_CLOCK_LFCLKSRC_SYNTH_250_PPM, NULL);
+
+    //debounce power
+    nrf_delay_ms(500);
+
+    //setup uart
+    os_uart_init();
+    os_printf("Welcome to sumisu - nrf52\r\n");
+}
