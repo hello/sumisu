935b2dfdbb7e9e5bce6d6d44b273749d6f8e153a
Jackson Chen <jchen@sayhello.com>
2015-12-04 17:04:06 -0800

    added new topics

--- include/os/pubsub_channels.h
+++ include/os/pubsub_channels.h
@@ -2,10 +2,12 @@
 #define PUBSUB_CHANNELS_H
 
 typedef enum{
-    PS_TEST_0,
-    PS_UART0_RX,
-    PS_BLE_CLI_RX,
-    PS_BLE_CLI_TX,
+    PS_SYSTEM,              //indicates system events
+    PS_TEST_0,              //for testing
+    PS_UART0_RX,            //uart input
+    PS_BLE_EVENTS,          //BLE events
+    PS_BLE_CLI_RX,          //BLE CLI Service input
+    PS_BLE_CLI_TX,          //BLE CLI service output
     PS_TOPIC_SIZE,
 }ps_topic_t;
 
